5385676748d0511a6e771d044b6d4baadfbbc6c1
Vandravon <vincent.drvdpt@gmail.com>
2023-01-12 11:34:21 +0100

    Adding exercice 2

--- Program.cs
+++ Program.cs
@@ -6,6 +6,14 @@ main();
 void main()
 {
 
+    // Exercice 2: tests (ATTENTION: repasser AddHouse en public pour que ça refonctionne)
+    // Village myVillage = new Village("Victor le createur");
+    // myVillage.GetName(); // affichera Victor le createur
+    // Console.WriteLine(myVillage.listHouse.Count); // affichera 1
+    // myVillage.AddHouse();
+    // myVillage.AddHouse();
+    // Console.WriteLine(myVillage.listHouse.Count); // affichera 3
+    // Console.WriteLine(myVillage.villageois); // affichera 30
 
 }
 
@@ -14,29 +22,38 @@ public class Village
     private string _name;
     private Ressources _myRessources;
     public House chefHome;
-    private int _villageois = 0;
+    public int villageois = 0;
+    public List<House> listHouse;
 
     public Village(string c_name)
     {
         _name = c_name;
         _myRessources = new Ressources();
-        _myHouse = new House();
-        _villageois += 10;
+        listHouse = new List<House> { chefHome };
+        villageois = 10 * listHouse.Count;
+
     }
 
-    public string GetName()
+    public void GetName()
     {
-        return _name;
+        Console.WriteLine($"Nom du village: {_name}");
     }
 
-    public int getWood()
+    public int GetWood()
     {
         return _myRessources.getWood();
     }
 
-    public int getStone()
+    public int GetStone()
     {
         return _myRessources.getStone();
+    }
+
+    private void AddHouse()
+    {
+        House houseToAdd = new House();
+        listHouse.Add(houseToAdd);
+        villageois = 10 * listHouse.Count;
     }
 
 

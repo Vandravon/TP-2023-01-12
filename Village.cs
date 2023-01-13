cef2cb99b83e36dd2d2808645629d884cd7d3afc
Vandravon <vincent.drvdpt@gmail.com>
2023-01-13 14:40:37 +0100

    Exercice 8 with a lot of fixes

--- Village.cs
+++ Village.cs
@@ -30,7 +30,7 @@ namespace myGame
 
             while (_gamePlayerChoice != "0" && _gamePlayerChoice != "")
             {
-                Console.WriteLine("");
+                Console.WriteLine("---------------");
                 Console.WriteLine("");
                 Console.WriteLine($"{getName()} || Pierre {getStone()} | Bois {getWood()} | Villageois {villageois}");
                 Console.WriteLine($"Ressources niveau {_myRessources.getLevel()} | Forêt niveau: {_myForest.getLevel()} | Mine niveau: {_myMine.getLevel()}");
@@ -38,95 +38,119 @@ namespace myGame
                 Console.WriteLine("Vos choix disponibles:");
                 Console.WriteLine("");
                 Console.WriteLine("1(nombre de villageois): Miner de la pierre | 2(nombre de villageois): Couper du bois | 3(nombre de maisons): Construire des maisons");
-                Console.WriteLine("4: Améliorer les ressources | 5: Améliorer la Mine | 6: Améliorer la Forêt");
+                Console.WriteLine("4: Améliorer les ressources | 5: Améliorer la Mine | 6: Améliorer la Forêt | 7: Regarder les alentours");
                 Console.WriteLine("0: Arrêter le jeu | 9: Triche");
                 Console.WriteLine("");
                 Console.WriteLine("Que voulez-vous faire?");
                 _gamePlayerChoice = Console.ReadLine();
-                Console.WriteLine("");
+                Console.WriteLine("---------------");
 
-
-                string playerParam = "";
-
-                // Permet de vérifier si le paramètre respecte le format (collé au choix avec des chiffres à l'intérieur)
-                if ((_gamePlayerChoice[0] == '1' || _gamePlayerChoice[0] == '2' || _gamePlayerChoice[0] == '3') && _gamePlayerChoice.Length > 3)
+                if (_gamePlayerChoice == "")
                 {
-                    if (_gamePlayerChoice[1] != '(' || _gamePlayerChoice[_gamePlayerChoice.Length - 1] != ')')
+                    _gamePlayerChoice = "-1";
+                    Console.WriteLine("Merci de remplir les champs");
+                }
+                else
+                {
+                    string playerParam = "";
+                    bool isAcceptablePlayerChoice = true;
+
+                    // Permet de vérifier si le paramètre respecte le format (collé au choix avec des chiffres à l'intérieur)
+                    if ((_gamePlayerChoice[0] == '1' || _gamePlayerChoice[0] == '2' || _gamePlayerChoice[0] == '3'))
                     {
-                        Console.WriteLine("Merci de signifier votre choix suivi du paramètre entre ()");
-                    }
-                    else
-                    {
-                        for (int i = 2; i < _gamePlayerChoice.Length - 1; i++)
+                        if (_gamePlayerChoice.Length < 4) // Vérifie que la réponse fait minimum 4 caractères
                         {
-                            if (_gamePlayerChoice[i] < '0' || _gamePlayerChoice[i] > '9')
+                            Console.WriteLine("Merci de signifier votre choix suivi du paramètre entre () _ex: 2(5)");
+                            isAcceptablePlayerChoice = false;
+                        }
+                        else
+                        {
+                            if (_gamePlayerChoice[1] != '(' || _gamePlayerChoice[_gamePlayerChoice.Length - 1] != ')') // Vérifie qu'il y a bien les () pour entourer le paramètre
                             {
-                                Console.WriteLine("Le paramètre ne doit contenir que des nombres");
+                                Console.WriteLine("Merci de signifier votre choix suivi du paramètre entre () _ex: 2(5)");
+                                isAcceptablePlayerChoice = false;
                             }
                             else
                             {
-                                playerParam += _gamePlayerChoice[i];
+                                for (int i = 2; i < _gamePlayerChoice.Length - 1; i++) // Vérifie qu'il y a uniquement des chiffres dans les paramètres
+                                {
+                                    if (_gamePlayerChoice[i] < '0' || _gamePlayerChoice[i] > '9')
+                                    {
+                                        Console.WriteLine("Le paramètre ne doit contenir que des nombres");
+                                        isAcceptablePlayerChoice = false;
+                                    }
+                                    else
+                                    {
+                                        playerParam += _gamePlayerChoice[i];
+                                    }
+                                }
                             }
                         }
+
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Merci de signifier votre choix suivi du paramètre entre ()");
-
-                }
-
-                if (_gamePlayerChoice[0] == '1')
-                {
-                    if (playerParam != "")
+                    else
                     {
-                        int nombreVillageois = Convert.ToInt32(playerParam);
-                        mineStone(nombreVillageois);
+                        if (_gamePlayerChoice.Length > 1)
+                        {
+                            isAcceptablePlayerChoice = false;
+                        }
                     }
-                }
 
-                else if (_gamePlayerChoice[0] == '2')
-                {
-
-
-                    if (playerParam != "")
+                    if (isAcceptablePlayerChoice && _gamePlayerChoice[0] >= '0' && _gamePlayerChoice[0] != '8')
                     {
-                        int nombreVillageois = Convert.ToInt32(playerParam);
-                        cutWood(nombreVillageois);
+                        if (_gamePlayerChoice[0] == '1')
+                        {
+
+                            int nombreVillageois = Convert.ToInt32(playerParam);
+                            mineStone(nombreVillageois);
+                        }
+                        else if (_gamePlayerChoice[0] == '2')
+                        {
+                            int nombreVillageois = Convert.ToInt32(playerParam);
+                            cutWood(nombreVillageois);
+                        }
+                        else if (_gamePlayerChoice[0] == '3')
+                        {
+
+                            int nombreMaisons = Convert.ToInt32(playerParam);
+                            buildHouse(nombreMaisons);
+                        }
+                        else if (_gamePlayerChoice[0] == '4')
+                        {
+                            upgradeRessources();
+                        }
+                        else if (_gamePlayerChoice[0] == '5')
+                        {
+                            upgradeMine();
+                        }
+                        else if (_gamePlayerChoice[0] == '6')
+                        {
+                            upgradeForest();
+                        }
+                        else if (_gamePlayerChoice[0] == '7')
+                        {
+                            lookAround();
+                        }
+                        else if (_gamePlayerChoice[0] == '9')
+                        {
+                            triche();
+                        }
+                        else if (_gamePlayerChoice[0] == '0')
+                        {
+                            Console.WriteLine("Merci d'avoir joué à Village Creator!");
+                        }
                     }
-                }
-                else if (_gamePlayerChoice[0] == '3')
-                {
-                    if (playerParam != "")
+
+                    else
                     {
-                        int nombreMaisons = Convert.ToInt32(playerParam);
-                        buildHouse(nombreMaisons);
+                        Console.WriteLine("Choix invalide");
                     }
+
+
                 }
-                else if (_gamePlayerChoice[0] == '4')
-                {
-                    upgradeRessources();
-                }
-                else if (_gamePlayerChoice[0] == '5')
-                {
-                    upgradeMine();
-                }
-                else if (_gamePlayerChoice[0] == '6')
-                {
-                    upgradeForest();
-                }
-                else if (_gamePlayerChoice[0] == '9')
-                {
-                    triche();
-                }
-                else if (_gamePlayerChoice[0] == '0')
-                {
-                    Console.WriteLine("Merci d'avoir joué à Village Creator!");
-                }
-                else
-                {
-                    Console.WriteLine("Vous avez mal renseigné les informations");
-                }
+
+
+
 
             }
         }
@@ -175,7 +199,7 @@ namespace myGame
                     _myRessources.usedStone(nombreVillageois * Mine.stone_cost);
                     _myRessources.usedWood(nombreVillageois * Mine.wood_cost);
                     _myRessources.addStone(_myMine.mineStone(nombreVillageois));
-                    Console.WriteLine("Minage de pierre effectué effectué!");
+                    Console.WriteLine("Minage de pierre effectué!");
                 }
             }
             else
@@ -197,6 +221,7 @@ namespace myGame
                     _myRessources.usedStone(nombreVillageois * Forest.stone_cost);
                     _myRessources.usedWood(nombreVillageois * Forest.wood_cost);
                     _myRessources.addWood(_myForest.cutWood(nombreVillageois));
+                    Console.WriteLine("Coupage de bois effectué!");
                 }
             }
             else
@@ -233,7 +258,7 @@ namespace myGame
                     listHouse = newListHouse;
                     _myRessources.usedStone(numberHouses * House.stone_needed);
                     _myRessources.usedWood(numberHouses * House.wood_needed);
-
+                    Console.WriteLine("Maisons construites!");
                 }
             }
         }
@@ -241,6 +266,7 @@ namespace myGame
         public void upgradeRessources()
         {
             _myRessources.upgrade();
+            Console.WriteLine("Ressources améliorées");
         }
 
         public void upgradeMine()
@@ -249,6 +275,7 @@ namespace myGame
             {
                 _myRessources.usedStone(10 * Mine.gain_stone);
                 _myMine.upgrade();
+                Console.WriteLine("Mine améliorée");
             }
             else
             {
@@ -263,6 +290,7 @@ namespace myGame
             {
                 _myRessources.usedWood(10 * Forest.gain_wood);
                 _myForest.upgrade();
+                Console.WriteLine("Forêt améliorée");
             }
             else
             {
@@ -270,10 +298,14 @@ namespace myGame
             }
         }
 
+        public void lookAround()
+        {
+            _myRessources.lookAround();
+            Console.WriteLine("Vous regardez les alentours, et ramassez quelques maigres ressources");
+        }
 
 
-
-        //// METHODES TESTS!!! ------ DELETE SOON
+        //// Tricher c'est mal, mais ça sert pour les tests
 
         public void triche()
         {
